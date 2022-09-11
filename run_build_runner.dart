07acfaa7db3ba1e9b639fd2d24f82f338a4a1d3e
zzq <i@zhangzqs.cn>
2022-09-11 22:48:32 +0800

    Update: run_build_runner.dart

--- run_build_runner.dart
+++ run_build_runner.dart
@@ -4,14 +4,16 @@ const rootDirectory = '.';
 const kiteWeatherEntityDirectory =
     '$rootDirectory/modules/kite_feature/modules/kite_feature_weather/modules/kite_weather_entity';
 const kiteUserEntityDirectory = '$rootDirectory/modules/kite_user_entity';
-Future<void> generateCode({required String workingDirectory}) async {
-  await Process.start(
+
+Future<int> generateCode({required String workingDirectory}) async {
+  final result = await Process.start(
     'flutter pub get && flutter pub run build_runner build --delete-conflicting-outputs',
     [],
     workingDirectory: workingDirectory,
     runInShell: true,
-    mode: ProcessStartMode.detachedWithStdio,
+    mode: ProcessStartMode.inheritStdio,
   );
+  return await result.exitCode;
 }
 
 Future<void> main() async {
