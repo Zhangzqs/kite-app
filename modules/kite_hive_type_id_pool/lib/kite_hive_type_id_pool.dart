0ebffcdb2b58c63755c3fe25a4243f4b3f9597da
zzq <i@zhangzqs.cn>
2022-09-11 21:04:43 +0800

    Refactor: 分离kite_hive_type_id_pool

---
+++ modules/kite_hive_type_id_pool/lib/kite_hive_type_id_pool.dart
@@ -0,0 +1,20 @@
+library kite_hive_type_id_pool;
+
+/// 存放所有 hive 的自定义类型的typeId
+class HiveTypeIdPool {
+  static const librarySearchHistoryItem = 1;
+  static const authItem = 2; // 改为单用户后已不再使用该id
+  static const weatherItem = 3;
+  static const reportHistoryItem = 4;
+  static const balanceItem = 5;
+  static const courseItem = 6;
+  static const expenseItem = 7;
+  static const expenseTypeItem = 8;
+  static const contactItem = 9;
+  static const userEventTypeItem = 10;
+  static const userEventItem = 11;
+  static const gameTypeItem = 12;
+  static const gameRecordItem = 13;
+  static const functionTypeItem = 14;
+  static const timetableMetaItem = 15;
+}
