06f4445749d0472381682640b515d935472f9a16
zzq <i@zhangzqs.cn>
2022-09-12 10:40:30 +0800

    Add: 构建脚本支持对所有submodule进行pub get

--- run_build_runner.dart
+++ run_build_runner.dart
@@ -5,7 +5,7 @@ const kiteWeatherEntityDirectory =
     '$rootDirectory/modules/kite_feature/modules/kite_feature_weather/modules/kite_weather_entity';
 const kiteUserEntityDirectory = '$rootDirectory/modules/kite_user_entity';
 
-Future<int> get({required String workingDirectory}) async {
+Future<int> runPubGet({required String workingDirectory}) async {
   final result = await Process.start(
     'flutter',
     ['pub', 'get'],
@@ -28,12 +28,38 @@ Future<int> runBuildRunner({required String workingDirectory}) async {
 }
 
 Future<void> generateCode({required String workingDirectory}) async {
-  await get(workingDirectory: workingDirectory);
+  await runPubGet(workingDirectory: workingDirectory);
   await runBuildRunner(workingDirectory: workingDirectory);
 }
 
+/// 判断指定文件夹是否是dart package
+bool isDartPackage(Directory directory) {
+  final file = File('${directory.path}${Platform.pathSeparator}pubspec.yaml');
+  return file.existsSync();
+}
+
+/// 获取项目中所有的dart package
+Stream<Directory> getDartPackageDir({required String workingDirectory}) {
+  return Directory(workingDirectory)
+      .list(recursive: true)
+      .where((event) => event is Directory && isDartPackage(event))
+      .map((event) => event as Directory);
+}
+
+/// 获取小风筝的所有子模块
+Stream<Directory> getAllSubModules() {
+  return getDartPackageDir(workingDirectory: rootDirectory)
+      .where((event) => event.path.startsWith('$rootDirectory${Platform.pathSeparator}modules'));
+}
+
+Future<void> runPubGetAll() async {
+  final tasks = (await getAllSubModules().toSet()).map((e) => runPubGet(workingDirectory: e.path));
+  await Future.wait(tasks);
+}
+
 Future<void> main() async {
-  await generateCode(workingDirectory: rootDirectory);
-  await generateCode(workingDirectory: kiteWeatherEntityDirectory);
-  await generateCode(workingDirectory: kiteUserEntityDirectory);
+  // await generateCode(workingDirectory: rootDirectory);
+  // await generateCode(workingDirectory: kiteWeatherEntityDirectory);
+  // await generateCode(workingDirectory: kiteUserEntityDirectory);
+  await runPubGetAll();
 }
